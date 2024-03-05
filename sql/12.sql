4a5f58f5a3dc6349de38d2d91533cb75ad4a8cc2
henrylong612 <henrylong612@gmail.com>
2024-03-05 09:37:41 -0800

    complete

--- sql/12.sql
+++ sql/12.sql
@@ -4,3 +4,54 @@
  *
  * Write a SQL query that finds all action fanatics.
  */
+
+SELECT 
+customer_id,
+first_name,
+last_name
+FROM (
+    SELECT
+    customer_id,
+    first_name,
+    last_name,
+    inventory_id,
+    RANK() OVER (PARTITION BY customer_id ORDER BY rental_date DESC)
+    FROM customer
+    JOIN rental USING (customer_id)
+) a
+JOIN inventory USING (inventory_id)
+JOIN film USING (film_id)
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+WHERE rank <= 5
+GROUP BY 1,2,3
+HAVING sum(CASE WHEN name = 'Action' THEN 1 ELSE 0 END) >= 4
+ORDER BY customer_id;
+
+
+
+
+
+
+
+
+SELECT
+customer_id,
+first_name,
+last_name
+FROM customer cu
+JOIN LATERAL (
+    SELECT
+    rental_date, inventory_id
+    FROM rental r
+    WHERE r.customer_id = cu.customer_id
+    ORDER BY rental_date DESC
+    LIMIT 5
+) a ON TRUE
+JOIN inventory i USING (inventory_id)
+JOIN film f USING (film_id)
+JOIN film_category fc USING (film_id)
+JOIN category ca USING (category_id)
+GROUP BY 1,2,3
+HAVING sum(CASE WHEN name = 'Action' THEN 1 ELSE 0 END) >= 4
+ORDER BY customer_id;
--- sql/15.sql
+++ sql/15.sql
@@ -8,3 +8,15 @@
  * But there is also a view in the database that contains the correct formatting,
  * and you can SELECT from that VIEW instead of constructing the entire query manually.
  */
+
+SELECT
+title,
+STRING_AGG(initcap(first_name) || initcap(last_name), ', ') AS actors
+FROM actor
+JOIN film_actor USING (actor_id)
+JOIn film USING (film_id)
+JOIN film_category USING (film_id)
+JOIN category USINg (category_id)
+WHERE name = 'Documentary'
+AND rating = 'G'
+GROUP BY 1;
